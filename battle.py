703af323c4bb4052ade015c74e09ccc3e0949f5e
Iain Walker <jtfjyrx@gmail.com>
2020-09-12 15:07:52 +0100

    considering restructuring

--- battle.py
+++ battle.py
@@ -1,4 +1,21 @@
+import random
+
 def battle():
     while True:
-    #have a battle
-        pass
+        print ("Do you want to: ")
+        print ("1: Attack")
+        print ("2: Block")
+        print ("3: Use item")
+        act = input ("")
+        act = act.lower()
+        if act == "attack":
+            crit = random.randint(1,100)
+            dmg = random.randint(18, 30)
+            if crit == 23:
+                dmg == ("100")
+                print ("CRITICAL HIT! You do 100 damage")
+            else:
+                print ("You do " + dmg + " damage")
+
+        elif act == "defend":
+
